fa5842b8bda86922eff46218eda058c87a22d3e2
Khwilo Kabaka <khwilowatai@gmail.com>
2021-01-11 11:44:32 +0300

    Fetch repo data from GitHub API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import React from 'react';
+
 import './App.css';
 
 function App() {
+  React.useEffect(() => {
+    let isCurrent = true;
+    fetch('https://api.github.com/search/repositories?q=stars:%3E1&sort=stars')
+      .then((res) => res.json())
+      .then((data) => {
+        if (isCurrent) {
+          console.log(data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return () => {
+      isCurrent = false;
+    };
+  }, []);
+
   return (
     <div>
       <header>
