19048b5c5b11142ef95fb1ad4606a351bd5b24be
Khwilo Kabaka <khwilowatai@gmail.com>
2021-01-11 16:02:35 +0300

    Create pafe not found component

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const NotFound = () => (
+  <div>
+    <h1>404 | Not Found</h1>
+    <Link to='/'>Go to the Home page</Link>
+  </div>
+);
+
+export default NotFound;
