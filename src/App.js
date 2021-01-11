5a15386f21535e001c867c29f6285cdddf7c1d24
Khwilo Kabaka <khwilowatai@gmail.com>
2021-01-11 14:20:26 +0300

    Display the repos card as a grid

--- src/App.js
+++ src/App.js
@@ -20,21 +20,23 @@ function App() {
       <main>
         {loading ? <div>Loading...</div> : null}
         {error ? <div>Error fetching data</div> : null}
-        {response
-          ? response.items.map((item) => {
-              return (
-                <RepoCard
-                  key={item.id}
-                  name={item.name}
-                  description={item.description}
-                  repoURL={item.html_url}
-                  homepage={item.homepage}
-                  stargazersCount={item.stargazers_count}
-                  watchersCount={item.watchers_count}
-                />
-              );
-            })
-          : null}
+        <div className='repositories'>
+          {response
+            ? response.items.map((item) => {
+                return (
+                  <RepoCard
+                    key={item.id}
+                    name={item.name}
+                    description={item.description}
+                    repoURL={item.html_url}
+                    homepage={item.homepage}
+                    stargazersCount={item.stargazers_count}
+                    watchersCount={item.watchers_count}
+                  />
+                );
+              })
+            : null}
+        </div>
       </main>
     </div>
   );
