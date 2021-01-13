c3558d2e377ab1d3235727bd652b99062763d81b
Khwilo Kabaka <khwilowatai@gmail.com>
2021-01-13 11:45:27 +0300

    Load the repos data on initial mount

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import { bindActionCreators } from 'redux';
 
+import './App.css';
 import NotFound from './components/NotFound';
 import RepoDetail from './components/RepoDetail';
 import RepoListContainer from './components/RepoList';
-import { useFetchRemote } from './hooks';
+import * as repositoryActions from './redux/actions/repositoryActions';
 
-function App() {
-  const [loading, response, error] = useFetchRemote(
-    'https://api.github.com/search/repositories?q=stars:%3E1&sort=stars'
-  );
+function App({ repositories, actions }) {
+  React.useEffect(() => {
+    if (repositories.length === 0) {
+      actions.loadRepositories().catch((error) => {
+        alert('[FAILED TO LOAD FOLLOWERS]: ', error);
+      });
+    }
+  }, []);
 
   return (
     <div className='wrapper'>
@@ -20,15 +27,15 @@ function App() {
       </header>
 
       <main>
-        {loading ? <div>Loading...</div> : null}
-        {error ? <div>Error fetching data</div> : null}
+        {/* {loading ? <div>Loading...</div> : null}
+        {error ? <div>Error fetching data</div> : null} */}
         <Router>
           <Switch>
             <Route path='/repo/:id'>
               <RepoDetail />
             </Route>
             <Route exact path='/'>
-              <RepoListContainer response={response} />
+              <RepoListContainer repositories={repositories} />
             </Route>
             <Route component={NotFound} />
           </Switch>
@@ -38,4 +45,21 @@ function App() {
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    repositories: state.repositories,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      loadRepositories: bindActionCreators(
+        repositoryActions.loadRepositories,
+        dispatch
+      ),
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
